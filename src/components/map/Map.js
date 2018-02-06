d767b336011e4abb1a8ab4ae9cf57bfbccb53c1a
John Hwang <johnsies87@gmail.com>
2018-02-06 00:36:47 -0800

    Add headers.

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Header from '../common/Header';
 
 export default class Map extends React.Component {
   render() {
     return (
       <View>
+        <Header title="Venture Map" />
         <Text>MAPKIYAY</Text>
       </View>
     );
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Header from '../common/Header';
 
 export default class Profile extends React.Component {
   render() {
     return (
       <View>
+        <Header title="Profile" />
         <Text>Yiipykiyay</Text>
       </View>
     );
