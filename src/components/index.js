14ae2c766fe6836c112bcd6695c856d7f97a90ee
John Hwang <johnsies87@gmail.com>
2018-04-07 12:43:28 -0700

    Move friend/group actions and reducer connect to venturists index.

--- src/components/index.js
+++ src/components/index.js
@@ -1,42 +1,9 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-
 import Routes from '../config/routes';
-import { getFriends, getGroups } from '../actions/friends';
 // import { getVentures } from '../actions/ventures';
 
-class App extends React.Component {
-  componentWillMount() {
-    this.props.getFriends();
-    this.props.getGroups();
-  }
+export default class App extends React.Component {
   render() {
-    console.log(this.props.friends);
-    console.log(this.props.groups);
     return <Routes />
   }
 }
-
-function mapStateToProps(state) {
-  const { account, friends: { friends, groups } } = state;
-
-  return {
-    friends,
-    groups,
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({
-    getFriends,
-    getGroups,
-  }, dispatch);
-}
-
-const AppContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(App);
-
-export default AppContainer;
--- src/components/venturists/Venturists.js
+++ src/components/venturists/Venturists.js
@@ -1,90 +1,97 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { getFriends, getGroups } from '../../actions/friends';
 import { ScrollView, View, Text, Image, StyleSheet } from 'react-native';
-import { ListItem, Avatar, Subheader } from 'react-native-material-ui';
+import { ListItem, Avatar, Subheader, Icon } from 'react-native-material-ui';
 import Header from '../common/Header';
 import Venturist from './Venturist';
-import { Icon } from 'react-native-material-ui';
 import ActionButton from 'react-native-action-button';
 
 import { venturistStyles as style } from './style';
 
-const groups = [{
-    name: 'The Juice Biters',
-    image: 'http://wallpaper-gallery.net/images/beautiful-girls-hd-wallpapers-free-download/beautiful-girls-hd-wallpapers-free-download-11.jpg',
-    venturists: 'Joe, Ricky, Bobby',
-    totalVentures: 14,
-    level: 9,
-    favoriteCategory: 'happiest of hours',
-  }, {
-    name: 'Vagina Monologues',
-    image: 'http://wallpaper-gallery.net/images/beautiful-girls-hd-wallpapers-free-download/beautiful-girls-hd-wallpapers-free-download-11.jpg',
-    venturists: 'Candy, Dandy, Sandy',
-    totalVentures: 10,
-    level: 5,
-    favoriteCategory: 'happiest of hours',
-  }, {
-    name: 'Hourly Happiness',
-    image: 'http://wallpaper-gallery.net/images/beautiful-girls-hd-wallpapers-free-download/beautiful-girls-hd-wallpapers-free-download-11.jpg',
-    venturists: 'Nick, La, John',
-    totalVentures: 102,
-    level: 100,
-    favoriteCategory: 'happiest of hours',
-}];
+// const groups = [{
+//     name: 'The Juice Biters',
+//     image: 'http://wallpaper-gallery.net/images/beautiful-girls-hd-wallpapers-free-download/beautiful-girls-hd-wallpapers-free-download-11.jpg',
+//     venturists: 'Joe, Ricky, Bobby',
+//     totalVentures: 14,
+//     level: 9,
+//     favoriteCategory: 'happiest of hours',
+//   }, {
+//     name: 'Vagina Monologues',
+//     image: 'http://wallpaper-gallery.net/images/beautiful-girls-hd-wallpapers-free-download/beautiful-girls-hd-wallpapers-free-download-11.jpg',
+//     venturists: 'Candy, Dandy, Sandy',
+//     totalVentures: 10,
+//     level: 5,
+//     favoriteCategory: 'happiest of hours',
+//   }, {
+//     name: 'Hourly Happiness',
+//     image: 'http://wallpaper-gallery.net/images/beautiful-girls-hd-wallpapers-free-download/beautiful-girls-hd-wallpapers-free-download-11.jpg',
+//     venturists: 'Nick, La, John',
+//     totalVentures: 102,
+//     level: 100,
+//     favoriteCategory: 'happiest of hours',
+// }];
 
-const friends = [{
-    name: 'La Baik',
-    image: 'http://wallpaper-gallery.net/images/beautiful-girls-hd-wallpapers-free-download/beautiful-girls-hd-wallpapers-free-download-11.jpg',
-    title: 'Adventurer',
-    totalVentures: 85,
-    level: 99,
-    favoriteCategory: 'happiest of hours',
-  }, {
-    name: 'Nick Castaneda',
-    image: 'http://wallpaper-gallery.net/images/beautiful-girls-hd-wallpapers-free-download/beautiful-girls-hd-wallpapers-free-download-11.jpg',
-    title: 'Journeyman',
-    totalVentures: 70,
-    level: 78,
-    favoriteCategory: 'happiest of hours',
-  }, {
-    name: 'John Hwang',
-    title: 'Novice',
-    totalVentures: 2,
-    level: 2,
-    favoriteCategory: 'happiest of hours',
-}];
-
+// const friends = [{
+//     name: 'La Baik',
+//     image: 'http://wallpaper-gallery.net/images/beautiful-girls-hd-wallpapers-free-download/beautiful-girls-hd-wallpapers-free-download-11.jpg',
+//     title: 'Adventurer',
+//     totalVentures: 85,
+//     level: 99,
+//     favoriteCategory: 'happiest of hours',
+//   }, {
+//     name: 'Nick Castaneda',
+//     image: 'http://wallpaper-gallery.net/images/beautiful-girls-hd-wallpapers-free-download/beautiful-girls-hd-wallpapers-free-download-11.jpg',
+//     title: 'Journeyman',
+//     totalVentures: 70,
+//     level: 78,
+//     favoriteCategory: 'happiest of hours',
+//   }, {
+//     name: 'John Hwang',
+//     title: 'Novice',
+//     totalVentures: 2,
+//     level: 2,
+//     favoriteCategory: 'happiest of hours',
+// }];
+@connect(mapStateToProps, mapDispatchToProps)
 export default class Venturists extends React.Component {
+  componentWillMount() {
+    console.log("Mounting", this.props)
+    this.props.getFriends();
+    this.props.getGroups();
+  }
   render() {
+    console.log("PROPS", this.props)
+    const { friends, groups } = this.props;
     return (
       <View style={{flex: 1}}>
         <ScrollView>
           <View>
             <Subheader text="Posse" />
-            {groups.map((group, index) => (
+            {groups && groups.map((group, index) => (
               <Venturist
                 {...this.props}
                 key={index}
                 name={group.name}
-                image={group.image}
-                venturists={group.venturists}
-                totalVentures={group.totalVentures}
-                level={group.level}
-                favoriteCategory={group.favoriteCategory}
+                venturists="{group.venturists}"
+                totalVentures="{group.totalVentures}"
+                level="{group.level}"
+                favoriteCategory="{group.favoriteCategory}"
               />
             ))}
           </View>
           <View>
             <Subheader text="Onesies" />
-            {friends.map((friend, index) => (
+            {friends && friends.map((friend, index) => (
               <Venturist
                 {...this.props}
                 key={index}
-                image={friend.image}
-                name={friend.name}
-                title={friend.title}
-                totalVentures={friend.totalVentures}
-                level={friend.level}
-                favoriteCategory={friend.favoriteCategory}
+                name={friend.nickname}
+                title="{friend.title}"
+                totalVentures="{friend.totalVentures}"
+                level="{friend.level}"
+                favoriteCategory="{friend.favoriteCategory}"
               />
             ))}
           </View>
@@ -100,4 +107,19 @@ export default class Venturists extends React.Component {
       </View>
     );
   }
+}
+
+
+function mapStateToProps(state) {
+  const { account, friends: { friends, groups } } = state;
+  return {
+    friends, groups,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    getFriends,
+    getGroups,
+  }, dispatch);
 }
