f2ed7062259cff871324c114ac7dc9bf24373c0b
John Hwang <johnsies87@gmail.com>
2018-09-18 18:26:24 -0700

    Specify white/black status bar.

--- src/root.js
+++ src/root.js
@@ -1,20 +1,24 @@
-import { Provider } from 'react-redux';
-import { AsyncStorage } from 'react-native';
+import { Provider } from 'react-redux'
+import { AsyncStorage, StatusBar } from 'react-native'
 
-import configureStore from './config/store';
-import AppContainer from './components';
+import configureStore from './config/store'
+import AppContainer from './components'
 
 import { ThemeProvider } from 'react-native-material-ui'
 
-const store = configureStore();
+const store = configureStore()
 export default class ConnectedApp extends Component {
   render() {
     return (
       <Provider store={store}>
         <ThemeProvider>
+          <StatusBar
+            backgroundColor="white"
+            barStyle="dark-content"
+          />
           <AppContainer />
         </ThemeProvider>
       </Provider>
-    );
+    )
   }
 }
